14c393cc644e40ecefe390ed54b8cd2de0883e96
xelarich <richard-alex@hotmail.fr>
2024-06-18 11:11:16 +0200

    wip

---
+++ lib/page/activity/group/widget/dog_kennel.dart
@@ -0,0 +1,288 @@
+import 'package:anchor_scroll_controller/anchor_scroll_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:responsive/responsive.dart';
+import 'package:whiteforest_website/shared/utils.dart';
+
+class DogKennel extends StatelessWidget {
+  const DogKennel(this._scrollController, {super.key});
+
+  final AnchorScrollController _scrollController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnchorItemWrapper(
+      controller: _scrollController,
+      index: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          SizedBox(
+            width: double.infinity,
+            child: ResponsiveRow(
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                FlexWidget(
+                  sm: 12,
+                  md: 12,
+                  lg: 12,
+                  xl: 5,
+                  xxl: 5,
+                  xxxl: 5,
+                  child: ClipRRect(
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(16)),
+                    child: Image.asset(
+                      "assets/images/summer/${getPathImage(context)}chenil.webp",
+                      width: 400,
+                      height: 450,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                FlexWidget(
+                  sm: 12,
+                  md: 12,
+                  lg: 12,
+                  xl: 6,
+                  xxl: 6,
+                  xxxl: 6,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //Titre et temps
+                        SizedBox(
+                          width: double.infinity,
+                          child: ResponsiveRow(
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            alignment: WrapAlignment.spaceBetween,
+                            children: [
+                              FlexWidget(
+                                xs: 10,
+                                sm: 10,
+                                md: 9,
+                                lg: 9,
+                                xl: 9,
+                                xxl: 9,
+                                xxxl: 9,
+                                child: Text.rich(
+                                  TextSpan(
+                                    text: "Visite du chenil".toUpperCase(),
+                                    style: const TextStyle(
+                                      fontSize: 28,
+                                      fontFamily: 'WickedGrit',
+                                    ),
+                                  ),
+                                  maxLines: 2,
+                                ),
+                              ),
+                              FlexWidget(
+                                xs: 4,
+                                sm: 2,
+                                md: 2,
+                                lg: 2,
+                                xl: 2,
+                                xxl: 2,
+                                xxxl: 2,
+                                child: Container(
+                                  width: double.infinity,
+                                  margin:
+                                      const EdgeInsets.symmetric(vertical: 8),
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue.shade50,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: const Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(right: 8.0),
+                                        child: Icon(
+                                          LineAwesomeIcons.clock,
+                                          size: 18,
+                                        ),
+                                      ),
+                                      Text("1H"),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        const Text.rich(
+                          TextSpan(
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Roboto',
+                            ),
+                            children: [
+                              TextSpan(
+                                  text:
+                                      "Un endroit conçu pour nos partenaires canin, environ 1h00 avec plus ou moins "),
+                              TextSpan(
+                                text: "80 loulous ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                  text:
+                                      "demandeurs de câlin et d’attention.\n"),
+                              TextSpan(text: "Des mushers "),
+                              TextSpan(
+                                text: "passionnés ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                  text: "prêt à vous expliquer leur travail, "
+                                      "leur passion et la vie de leurs chiens.\n"),
+                              TextSpan(
+                                  text:
+                                      "Apprenez en plus sur le métier de musher, sur l’éducation, l'entraînement et "),
+                              TextSpan(
+                                text: "les différents aspects ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(text: "de notre métier.\n"),
+                            ],
+                          ),
+                          textAlign: TextAlign.start,
+                          maxLines: 15,
+                        ),
+                        const Text(
+                          'Les tarifs :',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: Colors.brown.shade200,
+                              )),
+                          child: const Text(
+                            "15€/ Adultes\n"
+                            "10€/ Enfants de -12ans",
+                            style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                              height: 1.4,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: Colors.brown.shade200,
+                )),
+            child: ResponsiveRow(
+              crossAxisAlignment: WrapCrossAlignment.start,
+              children: [
+                FlexWidget(
+                  xs: 12,
+                  sm: 12,
+                  md: 12,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 4),
+                            child: Icon(Icons.info_outline),
+                          ),
+                          Text(
+                            'Lieu de pratique',
+                            style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 28.0),
+                        child: Text(
+                          "La Toussuire\n",
+                          style: TextStyle(fontFamily: 'Roboto', fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                FlexWidget(
+                  xs: 12,
+                  sm: 12,
+                  md: 12,
+                  lg: 8,
+                  xl: 8,
+                  xxl: 8,
+                  xxxl: 8,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 4),
+                            child: Icon(Icons.info_outline),
+                          ),
+                          Text(
+                            'Informations et recommandations',
+                            style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 28.0),
+                        child: Text.rich(TextSpan(
+                          style: TextStyle(fontFamily: 'Roboto', fontSize: 16),
+                          children: [
+                            TextSpan(text: "Équipement: "),
+                            TextSpan(
+                                text: "Vêtements confortables ",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text:
+                                    "qui ne craignent pas les poils et les traces de pattes, chaussure fermée, eau"),
+                          ],
+                        )),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
