b3007df8b12868457d6a056040cb69099632a22f
xelarich <richard-alex@hotmail.fr>
2024-07-31 16:46:51 +0200

    wip

--- lib/page/contact/contact_page.dart
+++ lib/page/contact/contact_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart' hide NavigationDrawer;
 import 'package:get_it/get_it.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:whiteforest_website/component/drawer/drawer_mobile.dart';
 import 'package:whiteforest_website/component/footer/footer.dart';
 import 'package:whiteforest_website/page/contact/widget/form_contact.dart';
 import 'package:whiteforest_website/service/conf_service.dart';
 import 'package:whiteforest_website/shared/utils.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   static const routeName = '/contact';
-  final ConfService confService = GetIt.I.get<ConfService>();
 
   ContactPage({super.key});
 
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final ConfService confService = GetIt.I.get<ConfService>();
+
   final GlobalKey<ScaffoldState> _key = GlobalKey();
+
+  late final GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.25489065226392, 6.27370834350585);
 
   @override
   Widget build(BuildContext context) {
@@ -29,15 +40,32 @@ class ContactPage extends StatelessWidget {
                 height: MediaQuery.of(context).size.height - 70,
                 color: Colors.brown.shade50,
               ),
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: const FormContact(),
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(32),
+                    child: const FormContact(),
+                  ),
+
+                ],
               ),
+
             ],
           ),
+
           const Footer(),
         ],
       ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
   }
 }
--- lib/page/contact/widget/form_contact.dart
+++ lib/page/contact/widget/form_contact.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:whiteforest_website/page/contact/widget/text_form_field_contact.dart';
@@ -27,6 +28,10 @@ class _FormContactState extends State<FormContact> {
   final TextEditingController _messageController = TextEditingController();
 
   final ContactProvider contactProvider = ContactProvider();
+
+  late final GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.25489065226392, 6.27370834350585);
 
   @override
   Widget build(BuildContext context) {
@@ -101,115 +106,143 @@ class _FormContactState extends State<FormContact> {
                 ],
               );
             }
-            return Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 24, horizontal: 16),
-                    child: Text('Contactez-nous !'.toUpperCase(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: ResponsiveValue<double>(
-                            context,
-                            defaultValue: 28,
-                            conditionalValues: [
-                              const Condition<double>.largerThan(
-                                  name: MOBILE, value: 38)
-                            ],
-                          ).value,
-                          fontFamily: 'WickedGrit',
-                        )),
-                  ),
-                  TextFormFieldContact(
-                    _nameController,
-                    'Entrez votre nom',
-                    'Nom',
-                    (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Veuillez entrer votre nom';
-                      }
-                      return null;
-                    },
-                    maxLines: 1,
-                  ),
-                  TextFormFieldContact(
-                    _mailController,
-                    'Entrez votre mail',
-                    'Mail',
-                    (value) {
-                      if (value == null || !value.isValidEmail()) {
-                        return 'Veuillez entrer votre mail';
-                      }
-                      return null;
-                    },
-                    maxLines: 1,
-                  ),
-                  //DropdownTextFieldContact(),
-                  SizedBox(
-                    height: 200,
-                    child: TextFormFieldContact(
-                      _messageController,
-                      'Écrivez ici votre message',
-                      'Message',
-                      (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Veuillez entrer un message';
-                        }
-                        return null;
-                      },
-                      maxLines: 10,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(150, 50),
-                        backgroundColor: Colors.brown,
-                        disabledBackgroundColor: Colors.brown,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0),
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 24, horizontal: 16),
+                          child: Text('Contactez-nous !'.toUpperCase(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: ResponsiveValue<double>(
+                                  context,
+                                  defaultValue: 28,
+                                  conditionalValues: [
+                                    const Condition<double>.largerThan(
+                                        name: MOBILE, value: 38)
+                                  ],
+                                ).value,
+                                fontFamily: 'WickedGrit',
+                              )),
                         ),
-                      ),
-                      onPressed: provider.isLoading
-                          ? null
-                          : () async {
-                              if (_formKey.currentState!.validate()) {
-                                contactProvider.sendMail(
-                                  _nameController.text,
-                                  _mailController.text,
-                                  _messageController.text,
-                                  () {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content: Text(
-                                            'Erreur lors de l\'envoi du message !'),
-                                      ),
-                                    );
-                                  },
-                                );
+                        TextFormFieldContact(
+                          _nameController,
+                          'Entrez votre nom',
+                          'Nom',
+                          (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer votre nom';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                        ),
+                        TextFormFieldContact(
+                          _mailController,
+                          'Entrez votre mail',
+                          'Mail',
+                          (value) {
+                            if (value == null || !value.isValidEmail()) {
+                              return 'Veuillez entrer votre mail';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                        ),
+                        //DropdownTextFieldContact(),
+                        SizedBox(
+                          height: 200,
+                          child: TextFormFieldContact(
+                            _messageController,
+                            'Écrivez ici votre message',
+                            'Message',
+                            (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Veuillez entrer un message';
                               }
+                              return null;
                             },
-                      child: provider.isLoading
-                          ? const Center(
-                              child: CircularProgressIndicator(
-                              color: Colors.white,
-                            ))
-                          : const Text(
-                              'Envoyer',
-                              style: TextStyle(color: Colors.white),
+                            maxLines: 10,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(150, 50),
+                              backgroundColor: Colors.brown,
+                              disabledBackgroundColor: Colors.brown,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
                             ),
+                            onPressed: provider.isLoading
+                                ? null
+                                : () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      contactProvider.sendMail(
+                                        _nameController.text,
+                                        _mailController.text,
+                                        _messageController.text,
+                                        () {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            const SnackBar(
+                                              content: Text(
+                                                  'Erreur lors de l\'envoi du message !'),
+                                            ),
+                                          );
+                                        },
+                                      );
+                                    }
+                                  },
+                            child: provider.isLoading
+                                ? const Center(
+                                    child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ))
+                                : const Text(
+                                    'Envoyer',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      scrollGesturesEnabled: false,
+                      initialCameraPosition: CameraPosition(
+                        target: _center,
+                        zoom: 16.0,
+                      ),
+                      onTap: (LatLng latLng) {
+
+                      },
+                    ),
+                  ),
+                ),
+              ],
             );
           },
         ),
       ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 }
