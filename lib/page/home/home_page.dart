8cbb0072f59685dc5982ad3e685234c7651e47cc
xelarich <richard-alex@hotmail.fr>
2025-02-20 13:44:04 +0100

    wip delete racket

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -18,6 +18,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Text('Site en maintenance',
+        style: TextStyle(fontSize: 48),),
+      ),
+    );
+    return Scaffold(
       backgroundColor: Colors.brown.shade50,
       key: _key,
       appBar: getTopBar(context, _key, HomePage.routeName),
